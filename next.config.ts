3624447d6a3abfd825626f2c21b66b9990930d49
summer-bst <453421041@qq.com>
2024-10-31 18:43:07 +0800

    ci: 项目初始化，完成基本构建配置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/next-components",
+  assetPrefix: "/next-components",
 };
 
 export default nextConfig;
